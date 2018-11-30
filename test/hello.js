64582d3467743b080e48ad8deb50aeab5f9f30ff
WenSan <937550637@qq.com>
2018-11-30 17:21:32 +0800

    add get post
    get post

--- test/hello.js
+++ test/hello.js
@@ -3,8 +3,15 @@ console.log('test -— hello world!')
 let express = require('express');
 let app = express();
  
-app.get('/', function (req, res) {
-   res.send('Hello World');
+app.get('api/:id', function (req, res) {
+   console.log('get res params',res.params)
+})
+
+app.post('/push',（req,res) =>{
+  console.log('---req:',req)
+  console.log('---res:',res)
+ 
+  //res.send(res)
 })
  
 let server = app.listen(8081, function () {
